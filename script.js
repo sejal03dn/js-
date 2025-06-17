cc8fb5a3e7f8a02dfe80ea55bb050ca978d0cfca
sejal bisen <sejalbisen961@gmail.com>
2025-06-17 15:13:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,38 @@
-$(document).ready(function () { 
-	var currentGfgStep, nextGfgStep, previousGfgStep; 
-	var opacity; 
-	var current = 1; 
-	var steps = $("fieldset").length; 
+// script.js
+function checkPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (
+        let i = 2;
+        i <= Math.sqrt(num); i++
+    ) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-	setProgressBar(current); 
+document
+    .getElementById("sub-id")
+    .addEventListener("click",
+        function () {
+            let num =
+                document.getElementById("input-id").value;
+            let result = checkPrime(num);
 
-	$(".next-step").click(function () { 
-
-		currentGfgStep = $(this).parent(); 
-		nextGfgStep = $(this).parent().next(); 
-
-		$("#progressbar li").eq($("fieldset") 
-			.index(nextGfgStep)).addClass("active"); 
-
-		nextGfgStep.show(); 
-		currentGfgStep.animate({ opacity: 0 }, { 
-			step: function (now) { 
-				opacity = 1 - now; 
-
-				currentGfgStep.css({ 
-					'display': 'none', 
-					'position': 'relative'
-				}); 
-				nextGfgStep.css({ 'opacity': opacity }); 
-			}, 
-			duration: 500 
-		}); 
-		setProgressBar(++current); 
-	}); 
-
-	$(".previous-step").click(function () { 
-
-		currentGfgStep = $(this).parent(); 
-		previousGfgStep = $(this).parent().prev(); 
-
-		$("#progressbar li").eq($("fieldset") 
-			.index(currentGfgStep)).removeClass("active"); 
-
-		previousGfgStep.show(); 
-
-		currentGfgStep.animate({ opacity: 0 }, { 
-			step: function (now) { 
-				opacity = 1 - now; 
-
-				currentGfgStep.css({ 
-					'display': 'none', 
-					'position': 'relative'
-				}); 
-				previousGfgStep.css({ 'opacity': opacity }); 
-			}, 
-			duration: 500 
-		}); 
-		setProgressBar(--current); 
-	}); 
-
-	function setProgressBar(currentStep) { 
-		var percent = parseFloat(100 / steps) * current; 
-		percent = percent.toFixed(); 
-		$(".progress-bar") 
-			.css("width", percent + "%") 
-	} 
-
-	$(".submit").click(function () { 
-		return false; 
-	}) 
-}); 
+            if (result) {
+                document.getElementById("dom-msg").innerHTML =
+                    num + " is a prime number.";
+            } else {
+                document.getElementById("dom-msg").innerHTML =
+                    num + " is not a prime number.";
+            }
+        }
+    );
+document.getElementById("id-clear")
+    .addEventListener("click", () => {
+        document.getElementById("input-id").value = "";
+        document.getElementById("dom-msg").innerHTML = "Result";
+    })
